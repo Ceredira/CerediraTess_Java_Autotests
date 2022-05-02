b83f2af91274fc3129fb3ad13be5745b6a0395b9
PycJIaH <ruslan.valeev@hotmail.com>
2022-05-02 18:36:20 +0300

    Первый кейс рабочий вариант

--- src/test/java/com/github/PycJIaH/Components/WebInterface/Authorization.java
+++ src/test/java/com/github/PycJIaH/Components/WebInterface/Authorization.java
@@ -24,38 +24,66 @@ public class Authorization {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         try {
-            //Перейти на страницу http://{url}:7801/
+            //1. Перейти на страницу http://{url}:7801/
             driver.get("http://192.168.242.128:7801/");
-            //Заголовок страницы равен "Вход в систему"
+            // Локатор поля "Логин"
+            WebElement login = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
+            // Локатор поля "Пароль"
+            WebElement password = driver.findElement(new By.ByXPath("//*[@id=\"password\"]"));
+            // Локатор Чек-бокса "Запомнить меня"
+            WebElement checkBox = driver.findElement(new By.ByXPath("//*[@id=\"remember\"]"));
+            // Локатор Кнопки "Войти"
+            WebElement submit = driver.findElement(new By.ByXPath("//*[@id=\"submit\"]"));
+            //1.1. Заголовок страницы равен "Вход в систему"
             String expectedTitle = "Вход в систему";
             String actualTitle = driver.getTitle();
             assertEquals(actualTitle, expectedTitle);
-            //Шапка страницы равна "Вход в систему"
+            //1.2. Шапка страницы равна "Вход в систему"
             String hatHomePage = "Вход в систему";
-            String actualHatHomePage = driver.findElement(new By.ByXPath("/html/body/div[1]/div[1]/div/h4/b")).getText();
+            String actualHatHomePage = driver.findElement(new By.ByXPath("//div[1]/h4/b")).getText();
             assertEquals(hatHomePage, actualHatHomePage);
-            // Поле "Логин"
-            WebElement login = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
-            //Есть поле "Логин"
-            assertTrue(login.isDisplayed(), "Поле Логин отсутствует");
-            //Есть поле "Пароль"
-            assertTrue(driver.findElement(new By.ByXPath("//*[@id=\"password\"]")).isDisplayed(), "Поле Пароль отсутстует");
-            //Есть неактивный чекбокс "Запомнить меня"
-            assertFalse(driver.findElement(new By.ByXPath("//*[@id=\"remember\"]")).isSelected(), "Чек бокс Запомнить меня выделен, хотя не должен быть");
-
-
-            //selectCheckBox(driver.findElement(new By.ByXPath("//*[@id=\"remember\"]")), false);
-
-            //В поле "Логин" вводим значение "admin"
+            //1.3. Есть поле "Логин"
+            assertTrue(login.isDisplayed(), "Поле \"Логин\" отсутствует");
+            //1.4. Есть поле "Пароль"
+            assertTrue(password.isDisplayed(), "Поле \"Пароль\" отсутстует");
+            //1.5. Есть неактивный чекбокс "Запомнить меня"
+            assertFalse(checkBox.isSelected(), "Чек бокс \"Запомнить меня\" выделен, хотя не должен быть");
+            //1.6. Есть кнопка "Войти"
+            assertTrue(submit.isDisplayed(), "Кнопка \"Войти\" отсутстует");
+            //2. В поле "Логин" вводим значение "admin"
             login.sendKeys("admin");
-            //В поле "Пароль" вводим значение "admin"
-            driver.findElement(new By.ByXPath("//*[@id=\"password\"]")).sendKeys("admin");
-            //Нажать на кнопку "Войти"
-            driver.findElement(new By.ByXPath("//*[@id=\"submit\"]")).click();
+            //3. В поле "Пароль" вводим значение "admin"
+            password.sendKeys("admin");
+            //4. Нажать на кнопку "Войти"
+            submit.click();
 
+            //Следующая страница:
+            //Локатор "Выполнение запросов"
+            WebElement performingRequests = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]/ul/li[1]/a"));
+//                        //Локатор "Блокировка агентов"
+            WebElement blockAgents = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a"));
+//            //Локатор кнопки "Обовить скрипты"
+            WebElement updateScriptsButton = driver.findElement(new By.ByXPath("//*[@id=\"updateCT\"]"));
+//            //Локатор кнопки "admin - Выход"
+            WebElement adminExitButton = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]/div/a"));
+//
+//            //4.1. В главном меню есть пункт "Выполнение запросов"
+            assertTrue(performingRequests.isDisplayed(), "Нет пункта \"Выполнение запросов\"");
+//            //4.2. В главном меню есть пункт "Блокировка агентов"
+            assertTrue(blockAgents.isDisplayed(), "Нет пункта \"Блокировка агентов\"");
+//            //4.3. Заголовок страницы равен "CerediraTess - Выполнение запросов"
+            String expectedTitle2 = "CerediraTess - Выполнение запросов";
+            String actualTitle2 = driver.getTitle();
+            assertEquals(actualTitle2, expectedTitle2);
+//            //4.4. На странице есть кнопка "Обновить скрипты"
+            assertTrue(updateScriptsButton.isDisplayed(), "Нет кнопки \"Обновить скрипты\"");
+//            //4.5. В главном меню есть пункт "admin - Выход"
+            assertTrue(adminExitButton.isDisplayed(),"Нет кнопки \"admin - Выход\"");
+//            //5. Нажать на пункт меню "admin - Выход"
+            adminExitButton.click();
 
         } finally {
-            driver.findElement(new By.ByLinkText("Выполнение запросов"));
+            driver.quit();
         }
     }
 
