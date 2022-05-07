e45c03956749e918625064d371f0362a4a7e12cf
PycJIaH <ruslan.valeev@hotmail.com>
2022-05-07 18:52:41 +0300

    Добавлены изменения

--- src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Users.java
+++ src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Users.java
@@ -2,14 +2,17 @@ package com.github.PycJIaH.Components.WebInterface.Auxiliary_functionality;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Users {
     WebDriver driver;
@@ -35,6 +38,45 @@ public class Users {
             password.sendKeys("admin");
             //4. Нажать на кнопку "Войти"
             submit.click();
+        } finally {
+            ;
+        }
+    }
+
+    public void createNewUser(String username) {
+
+        try {
+            //Локатор раздела "Администрирование":
+            WebElement Administration = driver.findElement(new By.ByXPath("//*[@id=\"navbarDropdown\"]"));
+            //Локатор раздела "Пользователи":
+            WebElement Users = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/div/a[4]"));
+            //2. В главном меню перейти в раздел "Администрирование -> Пользователи"
+            Administration.click();
+            Users.click();
+            //Локатор кнопки "Создать"
+            WebElement CreateButton = driver.findElement(new By.ByXPath("/html/body/div[1]/ul/li[2]/a"));
+            //3. Выбрать вкладку "Создать"
+            CreateButton.click();
+            //Локатор поля "Логин пользователя" в окне "Создать новую запись"
+            WebElement CreateNewLogin = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
+            //Локатор поля "Пароль" в окне "Создать новую запись"
+            WebElement CreateNewPassword = driver.findElement(new By.ByXPath("//*[@id=\"password\"]"));
+            //Локатор кнопки "Сохранить" в окне "Создать новую запись"
+            WebElement SaveButton = driver.findElement(new By.ByXPath("//*[@id=\"fa_modal_window\"]/div/div/form/fieldset/div[2]/input"));
+            //4.Ввести обязательные поля:
+            //4.1. В поле "Логин пользователя" ввести значение "test"
+            CreateNewLogin.sendKeys(username);
+            //4.2. В поле "Пароль" ввести значение "test"
+            CreateNewPassword.sendKeys("test");
+            //Временная строка удалить:
+            driver.findElement(new By.ByXPath("//*[@id=\"fs_uniquifier\"]")).sendKeys(username);
+            //5. Нажать на кнопку "Сохранить"
+            SaveButton.click();
+            //6. В таблице "Список" отображается строка со значением "test" в столбце "Логин пользователя":
+            String expectedUserName = "test";
+            String actualUserName = driver.findElement(new By.ByXPath("/html/body/div[1]/div[2]/table/tbody/tr[2]/td[3]")).getText();
+            assertEquals(expectedUserName, actualUserName);
+
         } finally {
             ;
         }
@@ -67,45 +109,44 @@ public class Users {
 
     @Test
     @DisplayName("2. Создание нового пользователя")
-    public void createNewUser() {
+    public void createNewUserTest() {
 
         try {
             //1. Войти на сайт с пользователем "admin"
             permanentAuthorization();
-            //Локатор раздела "Администрирование":
-            WebElement Administration = driver.findElement(new By.ByXPath("//*[@id=\"navbarDropdown\"]"));
-            //Локатор раздела "Пользователи":
-            WebElement Users = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/div/a[4]"));
-            //2. В главном меню перейти в раздел "Администрирование -> Пользователи"
-            Administration.click();
-            Users.click();
-            //Локатор кнопки "Создать"
-            WebElement CreateButton = driver.findElement(new By.ByXPath("/html/body/div[1]/ul/li[2]/a"));
-            //3. Выбрать вкладку "Создать"
-            CreateButton.click();
-            //Локатор поля "Логин пользователя" в окне "Создать новую запись"
-            WebElement CreateNewLogin = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
-            //Локатор поля "Пароль" в окне "Создать новую запись"
-            WebElement CreateNewPassword = driver.findElement(new By.ByXPath("//*[@id=\"password\"]"));
-            //Локатор кнопки "Сохранить" в окне "Создать новую запись"
-            WebElement SaveButton = driver.findElement(new By.ByXPath("//*[@id=\"fa_modal_window\"]/div/div/form/fieldset/div[2]/input"));
-            //4.Ввести обязательные поля:
-            //4.1. В поле "Логин пользователя" ввести значение "test"
-            CreateNewLogin.sendKeys("test");
-            //4.2. В поле "Пароль" ввести значение "test"
-            CreateNewPassword.sendKeys("test");
-            //Временная строка удалить:
-            driver.findElement(new By.ByXPath("//*[@id=\"fs_uniquifier\"]")).sendKeys("test");
-            //5. Нажать на кнопку "Сохранить"
-            SaveButton.click();
-            //6. В таблице "Список" отображается строка со значением "test" в столбце "Логин пользователя":
-            String expectedUserName = "test";
-            String actualUserName = driver.findElement(new By.ByXPath("/html/body/div[1]/div[2]/table/tbody/tr[2]/td[3]")).getText();
-            assertEquals(expectedUserName, actualUserName);
-
+            //Создание пользователя:
+            createNewUser("test");
 
         } finally {
             driver.quit();
+        }
+    }
+
+    @Test
+    @DisplayName("3. Удаление пользователя")
+    public void deleteNewUser() {
+
+        try {
+            //1. Войти на сайт с пользователем "admin"
+            permanentAuthorization();
+            String username = "test" + new Random().ints(2000,3000).findFirst().getAsInt();
+            //2. Создать пользователя "user"
+            createNewUser(username);
+            //Локатор кнопки "Удалить запись"
+            WebElement DeleteUserButton = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']/..//button[@title='Delete record']"));
+            //3. Нажать на иконку корзины "Удалить запись" пользователя с логином "user"
+            DeleteUserButton.click();
+            //4. Появилось модальное диалоговое окно с текстом "Вы уверены что хотите удалить эту запись?"
+            Alert ConfirmDelete = driver.switchTo().alert();
+            //5. Нажать на кнопку "ОК"
+            ConfirmDelete.accept();
+            //6. В таблице "Список" отсутствует строка со значением "user" в столбце "Логин пользователя"
+
+
+
+
+        } finally {
+            ;
         }
     }
 
