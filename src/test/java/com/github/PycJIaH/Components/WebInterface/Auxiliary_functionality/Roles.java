2e55d0d2da0916bac666827acffec7f5acc9d3e6
PycJIaH <ruslan.valeev@hotmail.com>
2022-05-18 14:33:39 +0300

    В классе "Роли" добавлен новый кейс: 3. Удаление созданной роли

--- src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Roles.java
+++ src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Roles.java
@@ -4,22 +4,87 @@ import com.google.common.cache.AbstractCache;
 import com.google.common.cache.Cache;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.nio.file.WatchEvent;
 import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Roles {
 
     WebDriver driver;
+
+    @Test
+    @DisplayName("1. Существование, при старте с нуля, роли по умолчанию \"admin\"")
+    public void existenceCheckRoleAdmin() {
+
+        try {
+            //1. Войти на сайт с пользователем "admin"
+            permanentAuthorization();
+            //Локатор раздела "Администрирование":
+            WebElement administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
+            //Локатор вкладки "Роли":
+            WebElement roles = driver.findElement(new By.ByXPath("//a[text()='Роли']"));
+            //2. В главном меню перейти в раздел "Администрирование -> Роли"
+            administration.click();
+            roles.click();
+            //3. В таблице "Список" отображается 1 строка со значением "admin" в столбце "Название роли"
+            //Проверка на кол-во записей в таблице
+            List<WebElement> countОfLines = driver.findElements(new By.ByXPath("//td[@class='col-name' and normalize-space()='admin']"));
+            int expectedCountList = 1;
+            int actualCountList = countОfLines.size();
+            assertEquals(expectedCountList, actualCountList);
+        } finally {
+            driver.quit();
+        }
+    }
+
+
+    @Test
+    @DisplayName("2. Создание роли")
+    public void createRoleTest() {
+
+        try {
+            permanentAuthorization();
+            createRole("tester_" + new Random().ints(1, 100).findFirst().getAsInt());
+
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    @DisplayName("3. Удаление созданной роли")
+    public void removeCreatedRole() {
+
+        try {
+            //1.Войти на сайт с пользователем "admin"
+            permanentAuthorization();
+            //2. Создать роль "tester_3"
+            String testerName = "tester_" + new Random().ints(101, 200).findFirst().getAsInt();
+            createRole(testerName);
+            //Локатор кнопки "Удалить запись"
+            WebElement deleteUserRoleButton = driver.findElement(new By.ByXPath("//td[normalize-space()='" + testerName + "']/..//button[@title='Delete record']"));
+            //Локатор имени пользователя вновь созданного (в ролях)
+            WebElement rolenameForDelete = driver.findElement(new By.ByXPath("//td[normalize-space()='" + testerName + "']"));
+            //3. Нажать на иконку "Удалить запись" роли с названием "tester_3"
+            deleteUserRoleButton.click();
+            //4. Появилось модальное диалоговое окно с текстом "Вы уверены что хотите удалить эту запись?"
+            Alert ConfirmDelete = driver.switchTo().alert();
+            //5. Нажать на кнопку "ОК"
+            ConfirmDelete.accept();
+            //6. В таблице "Список" отсутствует строка со значением "tester_3" в столбце "Название роли"
+            assertFalse(isElementExists(rolenameForDelete), "Пользователь не удален");
+
+        } finally {
+            driver.quit();
+        }
+    }
 
     private void permanentAuthorization() {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
@@ -48,38 +113,9 @@ public class Roles {
         }
     }
 
-    @Test
-    @DisplayName("1. Существование, при старте с нуля, роли по умолчанию \"admin\"")
-    public void existenceCheckRoleAdmin() {
+    public void createRole(String testerName) {
 
         try {
-            //1. Войти на сайт с пользователем "admin"
-            permanentAuthorization();
-            //Локатор раздела "Администрирование":
-            WebElement administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
-            //Локатор вкладки "Роли":
-            WebElement roles = driver.findElement(new By.ByXPath("//a[text()='Роли']"));
-            //2. В главном меню перейти в раздел "Администрирование -> Роли"
-            administration.click();
-            roles.click();
-            //3. В таблице "Список" отображается 1 строка со значением "admin" в столбце "Название роли"
-            //Проверка на кол-во записей в таблице
-            List<WebElement> countОfLines = driver.findElements(new By.ByXPath("//td[@class='col-name' and normalize-space()='admin']"));
-            int expectedCountList = 1;
-            int actualCountList = countОfLines.size();
-            assertEquals(expectedCountList, actualCountList);
-        } finally {
-            driver.quit();
-        }
-    }
-
-    @Test
-    @DisplayName("2. Создание роли")
-    public void createRole() {
-
-        try {
-            //1. Войти на сайт с пользователем "admin"
-            permanentAuthorization();
             //Локатор раздела "Администрирование":
             WebElement administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
             //Локатор вкладки "Роли":
@@ -114,7 +150,6 @@ public class Roles {
             //5. ...выбрать значение "admin"
             adminChoice.click();
             //6. В поле "Название роли" ввести значение "tester_2"
-            String testerName = "tester_" + new Random().ints(1, 100).findFirst().getAsInt();
             nameRoleField.sendKeys(testerName);
             //7. В поле "Описание" ввести значение "Тестирует систему"
             descriptionField.sendKeys("Тестирует систему");
@@ -124,7 +159,16 @@ public class Roles {
             assertTrue(driver.findElement(new By.ByXPath("//td[@class='col-name' and normalize-space()='" + testerName + "']")).isDisplayed());
 
         } finally {
-            driver.quit();
+
         }
+    }
+
+    private boolean isElementExists(WebElement el1) {
+        try {
+            el1.isDisplayed();
+            return true;
+        } catch (NoSuchElementException | StaleElementReferenceException e) {
+        }
+        return false;
     }
 }
--- src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Users.java
+++ src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Users.java
@@ -77,7 +77,7 @@ public class Users {
             assertEquals(expectedUserName, actualUserName);
 
         } finally {
-            ;
+
         }
     }
 
