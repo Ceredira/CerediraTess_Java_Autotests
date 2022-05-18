f327dc79534dc3e642ddc8b79a7c7464cf78a424
PycJIaH <ruslan.valeev@hotmail.com>
2022-05-18 13:50:29 +0300

    В классе "Роли" добавлен новый кейс: 2. Создание роли
    Также исправлены некоторые недочёты тестов

--- src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Roles.java
+++ src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Roles.java
@@ -9,8 +9,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.nio.file.WatchEvent;
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -18,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Roles {
 
     WebDriver driver;
+
     private void permanentAuthorization() {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
         driver = new ChromeDriver();
@@ -47,24 +50,79 @@ public class Roles {
 
     @Test
     @DisplayName("1. Существование, при старте с нуля, роли по умолчанию \"admin\"")
-    public void ExistenceCheckRoleAdmin() {
+    public void existenceCheckRoleAdmin() {
 
         try {
             //1. Войти на сайт с пользователем "admin"
             permanentAuthorization();
             //Локатор раздела "Администрирование":
-            WebElement Administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
+            WebElement administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
             //Локатор вкладки "Роли":
-            WebElement Roles = driver.findElement(new By.ByXPath("//a[text()='Роли']"));
+            WebElement roles = driver.findElement(new By.ByXPath("//a[text()='Роли']"));
             //2. В главном меню перейти в раздел "Администрирование -> Роли"
-            Administration.click();
-            Roles.click();
+            administration.click();
+            roles.click();
             //3. В таблице "Список" отображается 1 строка со значением "admin" в столбце "Название роли"
             //Проверка на кол-во записей в таблице
             List<WebElement> countОfLines = driver.findElements(new By.ByXPath("//td[@class='col-name' and normalize-space()='admin']"));
             int expectedCountList = 1;
             int actualCountList = countОfLines.size();
             assertEquals(expectedCountList, actualCountList);
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    @DisplayName("2. Создание роли")
+    public void createRole() {
+
+        try {
+            //1. Войти на сайт с пользователем "admin"
+            permanentAuthorization();
+            //Локатор раздела "Администрирование":
+            WebElement administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
+            //Локатор вкладки "Роли":
+            WebElement roles = driver.findElement(new By.ByXPath("//a[text()='Роли']"));
+            //2. В главном меню перейти в раздел "Администрирование -> Роли"
+            administration.click();
+            roles.click();
+            //Локатор кнопки "Создать"
+            WebElement createButton = driver.findElement(new By.ByXPath("//a[normalize-space()='Создать']"));
+            //3. Нажать на вкладку "Создать"
+            createButton.click();
+            //Локатор поля "Агенты"
+            WebElement agentField = driver.findElement(new By.ByXPath("//*[@id=\"s2id_agents\"]/ul"));
+            //Локатор поля "Пользователи"
+            WebElement usersField = driver.findElement(new By.ByXPath("//*[@id=\"s2id_users\"]/ul"));
+            //Локатор поля "Название роли"
+            WebElement nameRoleField = driver.findElement(new By.ByXPath("//*[@id=\"name\"]"));
+            //Локатор поля "Описание"
+            WebElement descriptionField = driver.findElement(new By.ByXPath("//*[@id=\"description\"]"));
+            //Локатор кнопки "Сохранить"
+            WebElement saveButton = driver.findElement(new By.ByXPath("//*[@id=\"fa_modal_window\"]/..//input[@value='Сохранить']"));
+            //4.В поле "Агенты"...
+            agentField.click();
+            //Локатор выпадающего элемента "CerediraTess"
+            WebElement dropDownElement = driver.findElement(new By.ByXPath("//*[contains(@id,\"select2-result-label-\") and text()='CerediraTess']"));
+            //4. ...выбрать значение "CerediraTess"
+            dropDownElement.click();
+            //5. В поле "Пользователи"...
+            usersField.click();
+            //Локатор значения "admin" в поле Пользователи
+            WebElement adminChoice = driver.findElement(new By.ByXPath("//*[contains(@id,\"select2-result-label-\") and text()='admin']"));
+            //5. ...выбрать значение "admin"
+            adminChoice.click();
+            //6. В поле "Название роли" ввести значение "tester_2"
+            String testerName = "tester_" + new Random().ints(1, 100).findFirst().getAsInt();
+            nameRoleField.sendKeys(testerName);
+            //7. В поле "Описание" ввести значение "Тестирует систему"
+            descriptionField.sendKeys("Тестирует систему");
+            //8. Нажать на кнопку "Сохранить"
+            saveButton.click();
+            //9. В таблице "Список" отображается строка со значением "tester_2" в столбце "Название роли"
+            assertTrue(driver.findElement(new By.ByXPath("//td[@class='col-name' and normalize-space()='" + testerName + "']")).isDisplayed());
+
         } finally {
             driver.quit();
         }
--- src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Users.java
+++ src/test/java/com/github/PycJIaH/Components/WebInterface/Auxiliary_functionality/Users.java
@@ -53,24 +53,24 @@ public class Users {
             Administration.click();
             Users.click();
             //Локатор кнопки "Создать"
-            WebElement CreateButton = driver.findElement(new By.ByXPath("//a[normalize-space()='Создать']"));
+            WebElement createButton = driver.findElement(new By.ByXPath("//a[normalize-space()='Создать']"));
             //3. Выбрать вкладку "Создать"
-            CreateButton.click();
+            createButton.click();
             //Локатор поля "Логин пользователя" в окне "Создать новую запись"
-            WebElement CreateNewLogin = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
+            WebElement createNewLogin = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
             //Локатор поля "Пароль" в окне "Создать новую запись"
-            WebElement CreateNewPassword = driver.findElement(new By.ByXPath("//*[@id=\"password\"]"));
+            WebElement createNewPassword = driver.findElement(new By.ByXPath("//*[@id=\"password\"]"));
             //Локатор кнопки "Сохранить" в окне "Создать новую запись"
-            WebElement SaveButton = driver.findElement(new By.ByXPath("//input[@type='submit']"));
+            WebElement saveButton = driver.findElement(new By.ByXPath("//input[@type='submit']"));
             //4.Ввести обязательные поля:
             //4.1. В поле "Логин пользователя" ввести значение "test"
-            CreateNewLogin.sendKeys(username);
+            createNewLogin.sendKeys(username);
             //4.2. В поле "Пароль" ввести значение "test"
-            CreateNewPassword.sendKeys("test");
+            createNewPassword.sendKeys("test");
             //Временная строка удалить:
             driver.findElement(new By.ByXPath("//*[@id=\"fs_uniquifier\"]")).sendKeys(username);
             //5. Нажать на кнопку "Сохранить"
-            SaveButton.click();
+            saveButton.click();
             //6. В таблице "Список" отображается строка со значением "test" в столбце "Логин пользователя":
             String expectedUserName = username;
             String actualUserName = driver.findElement(new By.ByXPath("//td[3][normalize-space()='" + username + "']")).getText();
@@ -89,12 +89,12 @@ public class Users {
             //1. Войти на сайт с пользователем "admin"
             permanentAuthorization();
             //Локатор раздела "Администрирование":
-            WebElement Administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
+            WebElement administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
             //Локатор раздела "Пользователи":
-            WebElement Users = driver.findElement(new By.ByXPath("//a[text()='Пользователи']"));
+            WebElement users = driver.findElement(new By.ByXPath("//a[text()='Пользователи']"));
             //2. В главном меню перейти в раздел "Администрирование -> Пользователи"
-            Administration.click();
-            Users.click();
+            administration.click();
+            users.click();
             //3. В списке есть 1 запись, где "Логин пользователя" равен "admin"
             String expectedUserName = "admin";
             String actualUserName = driver.findElement(new By.ByXPath("//tbody/tr/td[3]")).getText();
@@ -132,17 +132,17 @@ public class Users {
             String username = "test" + new Random().ints(2000, 3000).findFirst().getAsInt();
             createNewUser(username);
             //Локатор кнопки "Удалить запись"
-            WebElement DeleteUserButton = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']/..//button[@title='Delete record']"));
+            WebElement deleteUserButton = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']/..//button[@title='Delete record']"));
             //Локатор имени пользователя вновь созданного
-            WebElement UsernameForDelete = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']"));
+            WebElement usernameForDelete = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']"));
             //3. Нажать на иконку корзины "Удалить запись" пользователя с логином "user"
-            DeleteUserButton.click();
+            deleteUserButton.click();
             //4. Появилось модальное диалоговое окно с текстом "Вы уверены что хотите удалить эту запись?"
             Alert ConfirmDelete = driver.switchTo().alert();
             //5. Нажать на кнопку "ОК"
             ConfirmDelete.accept();
             //6. В таблице "Список" отсутствует строка со значением "user" в столбце "Логин пользователя"
-            assertFalse(isElementExists(UsernameForDelete), "Пользователь не удалён");
+            assertFalse(isElementExists(usernameForDelete), "Пользователь не удалён");
 
         } finally {
             driver.quit();
@@ -160,19 +160,19 @@ public class Users {
             String username = "user" + new Random().ints(2000, 3000).findFirst().getAsInt();
             createNewUser(username);
             //Локатор кнопки "Редактировать запись"
-            WebElement EditUserButton = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']/..//a[@title='Редактировать запись']"));
+            WebElement editUserButton = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']/..//a[@title='Редактировать запись']"));
             //3. Нажать на иконку "Редактировать запись" пользователя с логином "user"
-            EditUserButton.click();
+            editUserButton.click();
             //Локатор поля "Логин пользователя"
-            WebElement LoginUserEdit = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
+            WebElement loginUserEdit = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
             //4. Ввести в поле "Логин пользователя" значение "user2"
-            LoginUserEdit.clear(); //Очистить поле Логина
+            loginUserEdit.clear(); //Очистить поле Логина
             String editedusername = "user" + new Random().ints(3000, 4000).findFirst().getAsInt();
-            LoginUserEdit.sendKeys(editedusername);
+            loginUserEdit.sendKeys(editedusername);
             //Локатор кнопки "Сохранить"
-            WebElement SaveButtonEdit = driver.findElement(new By.ByXPath("//input[@type='submit']"));
+            WebElement saveButtonEdit = driver.findElement(new By.ByXPath("//input[@type='submit']"));
             //5. Нажать на кнопку "Сохранить"
-            SaveButtonEdit.click();
+            saveButtonEdit.click();
             //6. В таблице "Список" присутствует строка со значением "user2" в столбце "Логин пользователя"
             assertTrue(driver.findElement(new By.ByXPath("//td[normalize-space()='" + editedusername + "']")).isDisplayed(), "Отсутствует изменённый пользователь в списке");
 
@@ -189,12 +189,12 @@ public class Users {
             //1. Войти на сайт с пользователем "admin"
             permanentAuthorization();
             //Локатор раздела "Администрирование":
-            WebElement Administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
+            WebElement administration = driver.findElement(new By.ByXPath("//a[normalize-space()='Администрирование']"));
             //Локатор раздела "Пользователи":
-            WebElement Users = driver.findElement(new By.ByXPath("//a[text()='Пользователи']"));
+            WebElement users = driver.findElement(new By.ByXPath("//a[text()='Пользователи']"));
             //2. В главном меню перейти в раздел "Администрирование -> Пользователи"
-            Administration.click();
-            Users.click();
+            administration.click();
+            users.click();
             //Локатор кнопки "Просмотр записи"
             WebElement ViewUserButton = driver.findElement(new By.ByXPath("//td[normalize-space()='admin']/..//a[@title='Просмотр записи']"));
             //3. Нажать на иконку "Просмотр записи" пользователя с логином "admin"
@@ -204,55 +204,55 @@ public class Users {
             wait1.until(webDriver -> driver.findElement(new By.ByXPath("//h3[contains(text(), 'Просмотр записи')]")).isDisplayed());
             //4. Отобразились следующие поля и значения:
             //Локатор значения поля Логина пользователя:
-            WebElement LoginValue = driver.findElement(new By.ByXPath("//table//b[text()='Логин пользователя']/../../td[2]"));
+            WebElement loginValue = driver.findElement(new By.ByXPath("//table//b[text()='Логин пользователя']/../../td[2]"));
             //Локатор значения поля Блокировки:
-            WebElement BlockStatus = driver.findElement(new By.ByXPath("//table//b[text()='Блокировка']/../../td[2]"));
+            WebElement blockStatus = driver.findElement(new By.ByXPath("//table//b[text()='Блокировка']/../../td[2]"));
             //Локатор значения поля Почты:
-            WebElement EmailValue = driver.findElement(new By.ByXPath("//table//b[text()='Почта']/../../td[2]"));
+            WebElement emailValue = driver.findElement(new By.ByXPath("//table//b[text()='Почта']/../../td[2]"));
             //Локатор значения Даты создания:
-            WebElement CreateDate = driver.findElement(new By.ByXPath("//table//b[text()='Дата создания']/../../td[2]"));
+            WebElement createDate = driver.findElement(new By.ByXPath("//table//b[text()='Дата создания']/../../td[2]"));
             //Локатор значения Даты последнего обновления:
-            WebElement LastUpdateDate = driver.findElement(new By.ByXPath("//table//b[text()='Последнее обновление']/../../td[2]"));
+            WebElement lastUpdateDate = driver.findElement(new By.ByXPath("//table//b[text()='Последнее обновление']/../../td[2]"));
             //Локатор значения Роли
-            WebElement RoleValue = driver.findElement(new By.ByXPath("//table//b[text()='Роли']/../../td[2]"));
+            WebElement roleValue = driver.findElement(new By.ByXPath("//table//b[text()='Роли']/../../td[2]"));
             //4.1. Логин пользователя: admin
             assertTrue(driver.findElement(new By.ByXPath("//table//b[text()='Логин пользователя']")).isDisplayed(), "Поле \"Логин пользователя\" отсутствует");
-            assertTrue(LoginValue.isDisplayed(), "Значение логина отсутствует");
+            assertTrue(loginValue.isDisplayed(), "Значение логина отсутствует");
             String expectedLoginValue = "admin";
-            String actualLoginValue = LoginValue.getText();
+            String actualLoginValue = loginValue.getText();
             assertEquals(expectedLoginValue, actualLoginValue);
             //4.2. Блокировка: True
             assertTrue(driver.findElement(new By.ByXPath("//table//b[text()='Блокировка']")).isDisplayed(), "Поле \"Блокировка\" отсутствует");
-            assertTrue(BlockStatus.isDisplayed(), "Значение блокировки отсутствует");
+            assertTrue(blockStatus.isDisplayed(), "Значение блокировки отсутствует");
             String expectedBlockStatus = "True";
-            String actualBlockStatus = BlockStatus.getText();
+            String actualBlockStatus = blockStatus.getText();
             assertEquals(expectedBlockStatus, actualBlockStatus);
             //4.3. Почта: admin@admin.ru
             assertTrue(driver.findElement(new By.ByXPath("//table//b[text()='Почта']")).isDisplayed(), "Поле \"Почта\" отсутствует");
-            assertTrue(EmailValue.isDisplayed(), "Значение почты отсутствует");
+            assertTrue(emailValue.isDisplayed(), "Значение почты отсутствует");
             String expectedEmailValue = "admin@admin.ru";
-            String actualEmailValue = EmailValue.getText();
+            String actualEmailValue = emailValue.getText();
             assertEquals(expectedEmailValue, actualEmailValue);
             //4.4. Имя пользователя:
             assertTrue(driver.findElement(new By.ByXPath("//table//b[text()='Имя пользователя']")).isDisplayed(), "Поле \"Имя пользователя\" отсутствует");
             assertTrue(driver.findElement(new By.ByXPath("//table//b[text()='Имя пользователя']/../../td[2]")).isDisplayed(), "Значение имени отсутствует");
             //4.5. Дата создания: yyyy-mm-dd hh:tt:ss.ms
             assertTrue(driver.findElement(new By.ByXPath("//table//b[text()='Дата создания']")).isDisplayed(), "Поле \"Дата создания\" отсутствует");
-            assertTrue(CreateDate.isDisplayed(), "Значение даты создания отсутствует");
+            assertTrue(createDate.isDisplayed(), "Значение даты создания отсутствует");
             String expectedCreateDate = "2022-04-19 12:59:24.083932";
-            String actualCreateDate = CreateDate.getText();
+            String actualCreateDate = createDate.getText();
             assertEquals(expectedCreateDate, actualCreateDate);
             //4.6. Последнее обновление: yyyy-mm-dd hh:tt:ss.ms
             assertTrue(driver.findElement(new By.ByXPath("//table//b[text()='Последнее обновление']")).isDisplayed(), "Поле \"Последнее обновление\" отсутствует");
-            assertTrue(LastUpdateDate.isDisplayed(), "Значение даты последнего обновления отсутствует");
+            assertTrue(lastUpdateDate.isDisplayed(), "Значение даты последнего обновления отсутствует");
             String expectedLastUpdateDate = "2022-04-19 12:59:24.083932";
-            String actualLastUpdateDate = LastUpdateDate.getText();
+            String actualLastUpdateDate = lastUpdateDate.getText();
             assertEquals(expectedLastUpdateDate, actualLastUpdateDate);
             //4.7. Роли: admin
             assertTrue(driver.findElement(new By.ByXPath("//table//b[text()='Роли']")).isDisplayed(), "Поле \"Роли\" отсутствует");
-            assertTrue(RoleValue.isDisplayed(), "Значение Роли отсутствует");
+            assertTrue(roleValue.isDisplayed(), "Значение Роли отсутствует");
             String expectedRoleValue = "admin";
-            String actualRoleValue = RoleValue.getText();
+            String actualRoleValue = roleValue.getText();
             assertEquals(expectedRoleValue, actualRoleValue);
             //4.8. Последний вход - Поле отсутствует в данном релизе программы
             //4.9. Текущий вход - Поле отсутствует в данном релизе программы
@@ -277,25 +277,25 @@ public class Users {
             String username = "user" + new Random().ints(4000, 5000).findFirst().getAsInt();
             createNewUser(username);
             //Локатор имени пользователя вновь созданного
-            WebElement UsernameForDelete = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']"));
+            WebElement usernameForDelete = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']"));
             //Локатор чекбокса в таблице пользователей+
-            WebElement CheckBoxTable = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']/..//input[@type='checkbox']"));
+            WebElement checkBoxTable = driver.findElement(new By.ByXPath("//td[normalize-space()='" + username + "']/..//input[@type='checkbox']"));
             //3. Нажать на чекбокс слева пользователя "user"
-            CheckBoxTable.click();
+            checkBoxTable.click();
             //Локатор вкладки "С выбранным"
-            WebElement WithSelected = driver.findElement(new By.ByXPath("//a[text()='С выбранным']"));
+            WebElement withSelected = driver.findElement(new By.ByXPath("//a[text()='С выбранным']"));
             //4. Перейти во вкладку "С выбранным"
-            WithSelected.click();
+            withSelected.click();
             //Локатор пункта меню "Удалить"
-            WebElement DeleteUserButton = driver.findElement(new By.ByXPath("//a[text()='Удалить']"));
+            WebElement deleteUserButton = driver.findElement(new By.ByXPath("//a[text()='Удалить']"));
             //5. Выбрать пункт из меню "Удалить"
-            DeleteUserButton.click();
+            deleteUserButton.click();
             //6. Появилось модальное диалоговое окно с текстом "Вы уверены что хотите удалить ?"
             Alert ConfirmDelete = driver.switchTo().alert();
             //7. Нажать на "ОК"
             ConfirmDelete.accept();
             //8. В списке отсутствует строка со значением "user" в столбце "Логин пользователя"
-            assertFalse(isElementExists(UsernameForDelete), "Пользователь не удален");
+            assertFalse(isElementExists(usernameForDelete), "Пользователь не удален");
         } finally {
             driver.quit();
         }
