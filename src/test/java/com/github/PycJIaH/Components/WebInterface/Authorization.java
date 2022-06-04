882754348c0f6725dd063a09826f1c445a63e3cf
PycJIaH <ruslan.valeev@hotmail.com>
2022-06-04 14:58:16 +0300

    Ввели PageObject паттерн

--- src/test/java/com/github/PycJIaH/Components/WebInterface/Authorization.java
+++ src/test/java/com/github/PycJIaH/Components/WebInterface/Authorization.java
@@ -1,9 +1,7 @@
 package com.github.PycJIaH.Components.WebInterface;
 
-import com.github.PycJIaH.Components.WebInterface.Auxiliary_functionality.Roles;
-import com.github.PycJIaH.Components.WebInterface.Auxiliary_functionality.Users;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.github.PycJIaH.Components.WebInterface.Pages.LoginPage;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,240 +18,126 @@ public class Authorization {
     final Logger log = LoggerFactory.getLogger(Authorization.class);
     WebDriver driver;
 
-    @Test
-    @DisplayName("1. Проверка входа администратора")
-    public void check_admin_authorization() {
+    @BeforeAll
+    public static void beforeAll() {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
 
-        try {
-            log.info("1. Перейти на страницу http://{url}:7801/");
-            driver.get("http://192.168.242.128:7801/");
-            // Локатор поля "Логин"
-            WebElement login = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
-            // Локатор поля "Пароль"
-            WebElement password = driver.findElement(new By.ByXPath("//*[@id=\"password\"]"));
-            // Локатор Чек-бокса "Запомнить меня"
-            WebElement checkBox = driver.findElement(new By.ByXPath("//*[@id=\"remember\"]"));
-            // Локатор Кнопки "Войти"
-            WebElement submit = driver.findElement(new By.ByXPath("//*[@id=\"submit\"]"));
-            log.info("1.1. Заголовок страницы равен \"Вход в систему\"");
-            String expectedTitle = "Вход в систему";
-            String actualTitle = driver.getTitle();
-            assertEquals(actualTitle, expectedTitle);
-            log.info("1.2. Шапка страницы равна \"Вход в систему\"");
-            String hatHomePage = "Вход в систему";
-            String actualHatHomePage = driver.findElement(new By.ByXPath("//b[text()='Вход в систему']")).getText();
-            assertEquals(hatHomePage, actualHatHomePage);
-            log.info("1.3. Есть поле \"Логин\"");
-            assertTrue(login.isDisplayed(), "Поле \"Логин\" отсутствует");
-            log.info("1.4. Есть поле \"Пароль\"");
-            assertTrue(password.isDisplayed(), "Поле \"Пароль\" отсутстует");
-            log.info("1.5. Есть неактивный чекбокс \"Запомнить меня\"");
-            assertFalse(checkBox.isSelected(), "Чек бокс \"Запомнить меня\" выделен, хотя не должен быть");
-            log.info("1.6. Есть кнопка \"Войти\"");
-            assertTrue(submit.isDisplayed(), "Кнопка \"Войти\" отсутстует");
-            log.info("2. В поле \"Логин\" вводим значение \"admin\"");
-            login.sendKeys("admin");
-            log.info("3. В поле \"Пароль\" вводим значение \"admin\"");
-            password.sendKeys("admin");
-            log.info("4. Нажать на кнопку \"Войти\"");
-            submit.click();
+    @Test
+    @DisplayName("1. Проверка входа администратора")
+    public void check_admin_authorization() {
+        LoginPage lp = new LoginPage(driver);
 
-            //Следующая страница:
-            //Локатор "Выполнение запросов"
-            WebElement performingRequests = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]//a[normalize-space()='Выполнение запросов']"));
-            //Локатор "Блокировка агентов"
-            WebElement blockAgents = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]//a[normalize-space()='Блокировка агентов']"));
-            //Локатор кнопки "Обовить скрипты"
-            WebElement updateScriptsButton = driver.findElement(new By.ByXPath("//*[@id=\"updateCT\"]"));
-            //Локатор кнопки "admin - Выход"
-            WebElement adminExitButton = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]//a[normalize-space()='admin - Выход']"));
-            log.info("4.1. В главном меню есть пункт \"Выполнение запросов\"");
-            assertTrue(performingRequests.isDisplayed(), "Нет пункта \"Выполнение запросов\"");
-            log.info("4.2. В главном меню есть пункт \"Блокировка агентов\"");
-            assertTrue(blockAgents.isDisplayed(), "Нет пункта \"Блокировка агентов\"");
-            log.info("4.3. Заголовок страницы равен \"CerediraTess - Выполнение запросов\"");
-            String expectedTitle2 = "CerediraTess - Выполнение запросов";
-            String actualTitle2 = driver.getTitle();
-            assertEquals(actualTitle2, expectedTitle2);
-            log.info("4.4. На странице есть кнопка \"Обновить скрипты\"");
-            assertTrue(updateScriptsButton.isDisplayed(), "Нет кнопки \"Обновить скрипты\"");
-            log.info("4.5. В главном меню есть пункт \"admin - Выход\"");
-            assertTrue(adminExitButton.isDisplayed(), "Нет кнопки \"admin - Выход\"");
-            log.info("5. Нажать на пункт меню \"admin - Выход\"");
-            adminExitButton.click();
+        lp.load();
+        lp.checkTitle();
+        lp.checkHeader();
 
-        } finally {
-            driver.quit();
-        }
+        lp.usernameIsDisplayed();
+        lp.passwordIsDisplayed();
+        lp.rememberIsSelected();
+        lp.submitIsDisplayed();
+        lp.setUsername("admin");
+        lp.setPassword("admin");
+        lp.clickSubmit();
+
+        //Следующая страница:
+        //Локатор "Выполнение запросов"
+        WebElement performingRequests = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]//a[normalize-space()='Выполнение запросов']"));
+        //Локатор "Блокировка агентов"
+        WebElement blockAgents = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]//a[normalize-space()='Блокировка агентов']"));
+        //Локатор кнопки "Обовить скрипты"
+        WebElement updateScriptsButton = driver.findElement(new By.ByXPath("//*[@id=\"updateCT\"]"));
+        //Локатор кнопки "admin - Выход"
+        WebElement adminExitButton = driver.findElement(new By.ByXPath("//*[@id=\"navbarSupportedContent\"]//a[normalize-space()='admin - Выход']"));
+        log.info("4.1. В главном меню есть пункт \"Выполнение запросов\"");
+        assertTrue(performingRequests.isDisplayed(), "Нет пункта \"Выполнение запросов\"");
+        log.info("4.2. В главном меню есть пункт \"Блокировка агентов\"");
+        assertTrue(blockAgents.isDisplayed(), "Нет пункта \"Блокировка агентов\"");
+        log.info("4.3. Заголовок страницы равен \"CerediraTess - Выполнение запросов\"");
+        String expectedTitle2 = "CerediraTess - Выполнение запросов";
+        String actualTitle2 = driver.getTitle();
+        assertEquals(actualTitle2, expectedTitle2);
+        log.info("4.4. На странице есть кнопка \"Обновить скрипты\"");
+        assertTrue(updateScriptsButton.isDisplayed(), "Нет кнопки \"Обновить скрипты\"");
+        log.info("4.5. В главном меню есть пункт \"admin - Выход\"");
+        assertTrue(adminExitButton.isDisplayed(), "Нет кнопки \"admin - Выход\"");
+        log.info("5. Нажать на пункт меню \"admin - Выход\"");
+        adminExitButton.click();
     }
 
     @Test
     @DisplayName("2. Ошибка входа. Неправильный пароль")
     public void check_wrong_password_authorization() {
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        LoginPage lp = new LoginPage(driver);
 
-        try {
-            log.info("1. Перейти на страницу http://{url}:7801/");
-            driver.get("http://192.168.242.128:7801/");
-            // Локатор поля "Логин"
-            WebElement login = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
-            // Локатор поля "Пароль"
-            WebElement password = driver.findElement(new By.ByXPath("//*[@id=\"password\"]"));
-            // Локатор Кнопки "Войти"
-            WebElement submit = driver.findElement(new By.ByXPath("//*[@id=\"submit\"]"));
-            log.info("2. В поле \"Логин\" вводим значение \"admin\"");
-            login.sendKeys("admin");
-            log.info("3. В поле \"Пароль\" вводим значение \"1234\"");
-            password.sendKeys("1234");
-            log.info("4. Нажать на кнопку \"Войти\"");
-            submit.click();
-            log.info("5. Появилось сообщение об ошибке \"Invalid password\"");
-            String expectedErrorMessage = "Invalid password";
-            String actualErrorMessage = driver.findElement(new By.ByXPath("//li[text()='Invalid password']")).getText();
-            assertEquals(expectedErrorMessage, actualErrorMessage);
-            log.info("6. Шапка страницы равна \"Вход в систему\"");
-            String hatHomePage = "Вход в систему";
-            String actualHatHomePage = driver.findElement(new By.ByXPath("//b[text()='Вход в систему']")).getText();
-            assertEquals(hatHomePage, actualHatHomePage);
-
-        } finally {
-            driver.quit();
-        }
-
+        lp.load();
+        lp.setUsername("admin");
+        lp.setPassword("1234");
+        lp.clickSubmit();
+        log.info("5. Появилось сообщение об ошибке \"Invalid password\"");
+        String expectedErrorMessage = "Invalid password";
+        String actualErrorMessage = driver.findElement(new By.ByXPath("//li[text()='Invalid password']")).getText();
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+        lp.checkHeader();
     }
 
     @Test
     @DisplayName("3. Ошибка входа. Пользователь не существует")
     public void check_wrong_login_authorization() {
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        LoginPage lp = new LoginPage(driver);
 
-        try {
-            log.info("1. Перейти на страницу http://{url}:7801/");
-            driver.get("http://192.168.242.128:7801/");
-            // Локатор поля "Логин"
-            WebElement login = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
-            // Локатор поля "Пароль"
-            WebElement password = driver.findElement(new By.ByXPath("//*[@id=\"password\"]"));
-            // Локатор Кнопки "Войти"
-            WebElement submit = driver.findElement(new By.ByXPath("//*[@id=\"submit\"]"));
-            log.info("2. В поле \"Логин\" вводим значение \"admin23\"");
-            login.sendKeys("admin23");
-            log.info("3. В поле \"Пароль\" вводим значение \"1234\"");
-            password.sendKeys("1234");
-            log.info("4. Нажать на кнопку \"Войти\"");
-            submit.click();
-            log.info("5. Появилось сообщение об ошибке \"Specified user does not exist\"");
-            String expectedErrorMessage = "Specified user does not exist";
-            String actualErrorMessage = driver.findElement(new By.ByXPath("//li[text()='Specified user does not exist']")).getText();
-            assertEquals(expectedErrorMessage, actualErrorMessage);
-            log.info("6. Шапка страницы равна \"Вход в систему\"");
-            String hatHomePage = "Вход в систему";
-            String actualHatHomePage = driver.findElement(new By.ByXPath("//b[text()='Вход в систему']")).getText();
-            assertEquals(hatHomePage, actualHatHomePage);
-
-        } finally {
-            driver.quit();
-        }
-
+        lp.load();
+        lp.setUsername("admin23");
+        lp.setPassword("1234");
+        lp.clickSubmit();
+        log.info("5. Появилось сообщение об ошибке \"Specified user does not exist\"");
+        String expectedErrorMessage = "Specified user does not exist";
+        String actualErrorMessage = driver.findElement(new By.ByXPath("//li[text()='Specified user does not exist']")).getText();
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+        lp.checkHeader();
     }
 
     @Test
     @DisplayName("4. Ошибка входа. Параметр Логин пустой")
     public void check_login_empty_authorization() {
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        LoginPage lp = new LoginPage(driver);
 
-        try {
-            log.info("1. Перейти на страницу http://{url}:7801/");
-            driver.get("http://192.168.242.128:7801/");
-            // Локатор Кнопки "Войти"
-            WebElement submit = driver.findElement(new By.ByXPath("//*[@id=\"submit\"]"));
-            // Локатор поля тултипы "Заполнить это поле"
-            WebElement tooltip = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
-            log.info("2. Нажать на кнопку \"Войти\"");
-            submit.click();
-            log.info("3. Появилось сообщение об ошибке \"Заполните это поле\" - так как проверка текста не представляется возможным, данная проверка только на наличие тултипы \"Заполните это поле\"");
-            assertTrue(isAttributePresent(tooltip, "required"), "Атрибут required у поля Логин отсуствует");
-            log.info("4. Шапка страницы равна \"Вход в систему\"");
-            String hatHomePage = "Вход в систему";
-            String actualHatHomePage = driver.findElement(new By.ByXPath("//b[text()='Вход в систему']")).getText();
-            assertEquals(hatHomePage, actualHatHomePage);
-
-        } finally {
-            driver.quit();
-        }
-
+        lp.load();
+        lp.clickSubmit();
+        lp.usernameCheckRequired();
+        lp.checkHeader();
     }
 
     @Test
     @DisplayName("5. Ошибка входа. Параметр Пароль пустой")
     public void check_password_empty_authorization() {
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        LoginPage lp = new LoginPage(driver);
 
-        try {
-            log.info("1. Перейти на страницу http://{url}:7801/");
-            driver.get("http://192.168.242.128:7801/");
-            // Локатор поля "Логин"
-            WebElement login = driver.findElement(new By.ByXPath("//*[@id=\"username\"]"));
-            // Локатор Кнопки "Войти"
-            WebElement submit = driver.findElement(new By.ByXPath("//*[@id=\"submit\"]"));
-            // Локатор поля тултипы "Заполнить это поле"
-            WebElement tooltip = driver.findElement(new By.ByXPath("//*[@id=\"password\"]"));
-            log.info("2. В поле \"Логин\" вводим значение \"admin\"");
-            login.sendKeys("admin");
-            log.info("3. Нажать на кнопку \"Войти\"");
-            submit.click();
-            log.info("4. Появилось сообщение об ошибке \"Заполните это поле\" - так как проверка текста не представляется возможным, данная проверка только на наличие тултипы \"Заполните это поле\"");
-            assertTrue(isAttributePresent(tooltip, "required"), "Атрибут required у поля Пароль отсуствует");
-            log.info("5. Шапка страницы равна \"Вход в систему\"");
-            String hatHomePage = "Вход в систему";
-            String actualHatHomePage = driver.findElement(new By.ByXPath("//b[text()='Вход в систему']")).getText();
-            assertEquals(hatHomePage, actualHatHomePage);
-
-        } finally {
-            driver.quit();
-        }
-
+        lp.load();
+        lp.setUsername("admin");
+        lp.clickSubmit();
+        lp.passwordCheckRequired();
+        lp.checkHeader();
     }
 
-    private boolean isAttributePresent(WebElement element, String attribute) {
-        Boolean result = false;
-        try {
-            String value = element.getAttribute(attribute);
-            if (value != null) {
-                result = true;
+    @AfterEach
+    public void afterEach() {
+        if (driver != null) {
+            try {
+                log.info("Закрытие браузера");
+                driver.close();
+                driver.quit();
+            } catch (Exception ex) {
+                log.error(ex.getMessage());
             }
-        } catch (Exception e) {
         }
-        return result;
     }
-
-    private void selectCheckBox(WebElement el, boolean isSelected) {
-        boolean check = el.isSelected();
-        if (check != isSelected) {
-            el.click();
-        }
-    } //selectCheckBox(driver.findElement(new By.ByXPath("//*[@id=\"remember\"]")), true);
-
-//    @AfterEach
-//    public void close_chrome() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
 }
 
---
+++ src/test/java/com/github/PycJIaH/Components/WebInterface/Pages/LoginPage.java
@@ -0,0 +1,115 @@
+package com.github.PycJIaH.Components.WebInterface.Pages;
+
+import com.github.PycJIaH.Components.WebInterface.Authorization;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginPage {
+    protected WebDriver driver;
+
+    final Logger log = LoggerFactory.getLogger(LoginPage.class);
+
+    // Локатор поля "Логин"
+    private By usernameBy = new By.ByXPath("//*[@id=\"username\"]");
+
+    // Локатор поля "Пароль"
+    private By passwordBy = new By.ByXPath("//*[@id=\"password\"]");
+
+    // Локатор Чек-бокса "Запомнить меня"
+    private By rememberBy = new By.ByXPath("//*[@id=\"remember\"]");
+
+    // Локатор Кнопки "Войти"
+    private By submitBy = new By.ByXPath("//*[@id=\"submit\"]");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void load() {
+        log.info("Перейти на страницу http://{url}:7801/");
+        driver.get("http://192.168.242.128:7801/");
+    }
+
+    public void setUsername(String username) {
+        log.info("В поле \"Логин\" вводим значение \"" + username + "\"");
+        driver.findElement(usernameBy).sendKeys(username);
+    }
+
+    public void setPassword(String password) {
+        log.info("В поле \"Логин\" вводим значение \"" + password + "\"");
+        driver.findElement(passwordBy).sendKeys(password);
+    }
+
+    public void clickSubmit() {
+        log.info("Нажать на кнопку \"Войти\"");
+        driver.findElement(submitBy).click();
+    }
+
+    public void usernameIsDisplayed() {
+        log.info("Поле \"Логин\" видимо");
+        assertTrue(driver.findElement(usernameBy).isDisplayed(), "Поле \"Логин\" отсутствует");
+    }
+
+    public void passwordIsDisplayed() {
+        log.info("Поле \"Пароль\" видимо");
+        assertTrue(driver.findElement(passwordBy).isDisplayed(), "Поле \"Пароль\" отсутстует");
+    }
+
+    public void rememberIsSelected() {
+        log.info("Есть неактивный чекбокс \"Запомнить меня\"");
+        assertFalse(driver.findElement(rememberBy).isSelected(), "Чек бокс \"Запомнить меня\" выделен, хотя не должен быть");
+    }
+
+    public void submitIsDisplayed() {
+        log.info("Есть кнопка \"Войти\"");
+        assertTrue(driver.findElement(submitBy).isDisplayed(), "Кнопка \"Войти\" отсутстует");
+    }
+
+    public void usernameCheckRequired() {
+        log.info("Рядом с полем \"Логин\" появилось сообщение об ошибке \"Заполните это поле\" - так как проверка текста не представляется возможным, данная проверка только на наличие тултипы \"Заполните это поле\"");
+        assertTrue(isAttributePresent(driver.findElement(usernameBy), "required"), "Атрибут required у поля Логин отсуствует");
+    }
+
+    public void passwordCheckRequired() {
+        log.info("Рядом с полем \"Пароль\" появилось сообщение об ошибке \"Заполните это поле\" - так как проверка текста не представляется возможным, данная проверка только на наличие тултипы \"Заполните это поле\"");
+        assertTrue(isAttributePresent(driver.findElement(passwordBy), "required"), "Атрибут required у поля Пароль отсуствует");
+    }
+
+    public void checkTitle() {
+        log.info("Заголовок страницы равен \"Вход в систему\"");
+        String expectedTitle = "Вход в систему";
+        String actualTitle = driver.getTitle();
+        assertEquals(actualTitle, expectedTitle);
+    }
+
+    public void checkHeader() {
+        log.info("Шапка страницы равна \"Вход в систему\"");
+        String hatHomePage = "Вход в систему";
+        String actualHatHomePage = driver.findElement(new By.ByXPath("//b[text()='Вход в систему']")).getText();
+        assertEquals(hatHomePage, actualHatHomePage);
+    }
+
+    private boolean isAttributePresent(WebElement element, String attribute) {
+        Boolean result = false;
+        try {
+            String value = element.getAttribute(attribute);
+            if (value != null) {
+                result = true;
+            }
+        } catch (Exception e) {
+        }
+        return result;
+    }
+
+    private void selectCheckBox(WebElement el, boolean isSelected) {
+        boolean check = el.isSelected();
+        if (check != isSelected) {
+            el.click();
+        }
+    }
+}
